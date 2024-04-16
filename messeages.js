24dcd630da4496cb38ab887f75de09941d712633
Kameshwaran Murugan <kameshwaran@crayond.co>
2024-04-16 08:44:46 +0530

    classes and modules

---
+++ messeages.js
@@ -0,0 +1,7 @@
+export const welcomeMessage = "Hi all, welcome to React JS Class";
+
+const massageMessage = (message) => {
+    return `------------${message}---------------`
+}
+
+export default massageMessage
